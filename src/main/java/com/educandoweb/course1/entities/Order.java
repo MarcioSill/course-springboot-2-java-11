6246503009d09337775ca328da2cc34a93ff0e0d
MarcioSill <marcio.sill@hotmail.com>
2022-07-05 20:55:04 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course1/entities/Order.java
+++ src/main/java/com/educandoweb/course1/entities/Order.java
@@ -28,6 +28,7 @@ public class Order implements Serializable {
 	
 	private Integer orderStatus;
 	
+	//muitos para um ManyToOne
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
--- src/main/java/com/educandoweb/course1/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course1/repositories/UserRepository.java
@@ -1,10 +1,11 @@
 package com.educandoweb.course1.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.educandoweb.course1.entities.User;
 
-//@Repository
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     
 }
