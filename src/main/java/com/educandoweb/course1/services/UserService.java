90f66dcc082a2644ac526df09050a2c98a198b15
MarcioSill <marcio.sill@hotmail.com>
2022-08-15 22:18:24 -0300

    User update

--- src/main/java/com/educandoweb/course1/services/UserService.java
+++ src/main/java/com/educandoweb/course1/services/UserService.java
@@ -14,21 +14,35 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
+	// findById ele tras o objeto do banco de dados para poder trabalhar
 	public User findById(Long id) {
-		Optional<User>obj = repository.findById(id);
+		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
-	
+
 	public User insert(User obj) {
-		return repository.save(obj);		
+		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+
+	// getOne prepara o objeto monitorado para poder mexer e depois atualizar
+	public User update(Long id, User obj) {
+		User entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 }
